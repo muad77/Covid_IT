f2077b91c31c4c42175493b97a356749fbba0465
Rafał <rafal.nowak@lug.com.pl>
2020-09-28 21:28:25 +0200

    d

--- ebuilding-permits.js
+++ ebuilding-permits.js
@@ -74,7 +74,7 @@
   // Create event listeners for when the user submits the form
   $(document).ready(function () {
     $('#submitButton').click(function () {
-      tableau.connectionName = 'Eurostat'; // This will be the data source name in Tableau
+      tableau.connectionName = 'ebuilding-permits'; // This will be the data source name in Tableau
       tableau.submit(); // This sends the connector object to Tableau
     });
   });
