19429e6a6d6ed0dc18f51e5723cc6cf03172d243
Rafał <rafal.nowak@lug.com.pl>
2020-10-02 23:23:06 +0200

    Update Eurostat.js

--- Eurostat.js
+++ Eurostat.js
@@ -45,8 +45,7 @@
     // Download the data
     myConnector.getData = function (table, doneCallback) {
 
-      //	Gross domestic product, current prices
-  
+      //	Gross domestic product, current prices  
       JSONstat('https://cors-anywhere.herokuapp.com/' + 'http://ec.europa.eu/eurostat/wdds/rest/data/v2.1/json/en/teina010?na_item=B1GQ&precision=1&unit=MIO_EUR_SCA&s_adj=SCA').then(function (resp) {
         let data = resp.toTable({ type: 'array' });
         let columns = data.shift();
@@ -72,8 +71,7 @@
         table.appendRows(tableData);
       });
       
-      //	Current account balance - 3 year average
-  
+      //	Current account balance - 3 year average  
       JSONstat('https://cors-anywhere.herokuapp.com/' + 'http://ec.europa.eu/eurostat/wdds/rest/data/v2.1/json/en/tipsbp10?precision=1&unit=PC_GDP_3Y&partner=WRL_REST&stk_flow=BAL&s_adj=NSA&bop_item=CA').then(function (resp) {
         let data = resp.toTable({ type: 'array' });
         let columns = data.shift();
@@ -97,7 +95,80 @@
         table.appendRows(tableData);
       });
 
+      //	Building permits - quarterly data
+      JSONstat('https://cors-anywhere.herokuapp.com/' + 'http://ec.europa.eu/eurostat/wdds/rest/data/v2.1/json/en/sts_cobp_q?precision=1&unit=PCH_SM&s_adj=NSA&indic_bt=PNUM&nace_r2=F_CC11_X_CC113').then(function (resp) {
+        let data = resp.toTable({ type: 'array' });
+        let columns = data.shift();
+        let indexes = {};
+        let tableData = [];
+  
+        for (let c in columns) {
+          indexes[columns[c]] = +c;
+        }
+  
+        for (let row of data) {
+          tableData.push({
+            value: row[indexes.Value],
+            time: row[indexes.time],
+            geo: row[indexes.geo],
+            updated: resp.updated,
+            label: resp['label'],
+            unit: row[indexes.unit],
+          });
+        } 
+        table.appendRows(tableData);
+      });
 
+  //	Building productions
+  JSONstat('https://cors-anywhere.herokuapp.com/' + 'http://ec.europa.eu/eurostat/wdds/rest/data/v2.1/json/en/teiis500?precision=1&unit=PCH_M12_CA&unit=PCH_M1_SCA&indic_bt=PROD&nace_r2=F').then(function (resp) {
+    let data = resp.toTable({ type: 'array' });
+    let columns = data.shift();
+    let indexes = {};
+    let tableData = [];
+
+    for (let c in columns) {
+      indexes[columns[c]] = +c;
+    }
+
+    for (let row of data) {
+      tableData.push({
+        value: row[indexes.Value],
+        time: row[indexes.time],
+        geo: row[indexes.geo],
+        updated: resp.updated,
+        label: resp['label'],
+        unit: row[indexes.unit],
+      });
+    } 
+    table.appendRows(tableData);
+  });
+
+//	houses prices
+JSONstat('https://cors-anywhere.herokuapp.com/' + 'http://ec.europa.eu/eurostat/wdds/rest/data/v2.1/json/en/teiis500?precision=1&unit=PCH_M12_CA&unit=PCH_M1_SCA&indic_bt=PROD&nace_r2=F').then(function (resp) {
+  let data = resp.toTable({ type: 'array' });
+  let columns = data.shift();
+  let indexes = {};
+  let tableData = [];
+
+  for (let c in columns) {
+    indexes[columns[c]] = +c;
+  }
+
+  for (let row of data) {
+    tableData.push({
+      value: row[indexes.Value],
+      time: row[indexes.time],
+      geo: row[indexes.geo],
+      updated: resp.updated,
+      label: resp['label'],
+      unit: row[indexes.unit],
+    });
+  } 
+  table.appendRows(tableData);
+});
+
+
+      //	Gross domestic product, volumes (zmiana % kdk)
       JSONstat('https://cors-anywhere.herokuapp.com/' + 'http://ec.europa.eu/eurostat/wdds/rest/data/v2.1/json/en/teina011?na_item=B1GQ&precision=1&unit=PCH_Q4_SCA').then(function (resp) {
         let data = resp.toTable({ type: 'array' });
         let columns = data.shift();
