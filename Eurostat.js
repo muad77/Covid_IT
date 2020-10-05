ba576266c5b54283d2e3c651becc8d9d331bf578
Rafał <rafal.nowak@lug.com.pl>
2020-10-05 22:53:41 +0200

    Update Eurostat.js

--- Eurostat.js
+++ Eurostat.js
@@ -33,10 +33,10 @@
           dataType: tableau.dataTypeEnum.string,
           },
       ];
-      var tableSchema = {
-        id: 'Eurostat_data',
-        alias: 'Eurostat',
-        columns: cols,
+        var tableSchema = {
+          id: 'Eurostat_data',
+          alias: 'Eurostat',
+          columns: cols,
       };
   
       schemaCallback([tableSchema]);
@@ -44,9 +44,9 @@
   
     // Download the data
     myConnector.getData = function (table, doneCallback) {
-
+       
       //	Gross domestic product, current prices  
-      JSONstat('https://cors-anywhere.herokuapp.com/' + 'http://ec.europa.eu/eurostat/wdds/rest/data/v2.1/json/en/teina010?na_item=B1GQ&precision=1&unit=MIO_EUR_SCA&s_adj=SCA').then(function (resp) {
+      JSONstat('https://cors-anywhere.herokuapp.com/' + 'http://ec.europa.eu/eurostat/wdds/rest/data/v2.1/json/en/teina010?na_item=B1GQ&precision=1&unit=MIO_EUR_SCA&s_adj=SCA').then(function(resp) {
         let data = resp.toTable({ type: 'array' });
         let columns = data.shift();
         let indexes = {};
@@ -72,7 +72,7 @@
       });
       
       //	Current account balance - 3 year average  
-      JSONstat('https://cors-anywhere.herokuapp.com/' + 'http://ec.europa.eu/eurostat/wdds/rest/data/v2.1/json/en/tipsbp10?precision=1&unit=PC_GDP_3Y&partner=WRL_REST&stk_flow=BAL&s_adj=NSA&bop_item=CA').then(function (resp) {
+      JSONstat('https://cors-anywhere.herokuapp.com/' + 'http://ec.europa.eu/eurostat/wdds/rest/data/v2.1/json/en/tipsbp10?precision=1&unit=PC_GDP_3Y&partner=WRL_REST&stk_flow=BAL&s_adj=NSA&bop_item=CA').then(function(resp) {
         let data = resp.toTable({ type: 'array' });
         let columns = data.shift();
         let indexes = {};
@@ -94,9 +94,9 @@
         } 
         table.appendRows(tableData);
       });
-
+      
       //	Building permits - quarterly data
-      JSONstat('https://cors-anywhere.herokuapp.com/' + 'http://ec.europa.eu/eurostat/wdds/rest/data/v2.1/json/en/sts_cobp_q?precision=1&unit=PCH_SM&s_adj=NSA&indic_bt=PNUM&nace_r2=F_CC11_X_CC113').then(function (resp) {
+      JSONstat('https://cors-anywhere.herokuapp.com/' + 'http://ec.europa.eu/eurostat/wdds/rest/data/v2.1/json/en/sts_cobp_q?precision=1&unit=PCH_SM&s_adj=NSA&indic_bt=PNUM&nace_r2=F_CC11_X_CC113').then(function(resp) {
         let data = resp.toTable({ type: 'array' });
         let columns = data.shift();
         let indexes = {};
@@ -118,9 +118,9 @@
         } 
         table.appendRows(tableData);
       });
-
+     
       //	Production in construction"
-      JSONstat('https://cors-anywhere.herokuapp.com/' + 'http://ec.europa.eu/eurostat/wdds/rest/data/v2.1/json/en/teiis500?precision=1&unit=PCH_M12_CA&unit=PCH_M1_SCA&indic_bt=PROD&nace_r2=F').then(function (resp) {
+      JSONstat('https://cors-anywhere.herokuapp.com/' + 'http://ec.europa.eu/eurostat/wdds/rest/data/v2.1/json/en/teiis500?precision=1&unit=PCH_M12_CA&unit=PCH_M1_SCA&indic_bt=PROD&nace_r2=F').then(function (resp){
         let data = resp.toTable({ type: 'array' });
         let columns = data.shift();
         let indexes = {};
@@ -143,8 +143,9 @@
         table.appendRows(tableData);
       });
 
-//	houses prices
-      JSONstat('https://cors-anywhere.herokuapp.com/' + 'http://ec.europa.eu/eurostat/wdds/rest/data/v2.1/json/en/teicp270?indic=TOTAL&precision=1&unit=PCH_Q4_NSA').then(function (resp) {
+      
+      //	houses prices
+      JSONstat('https://cors-anywhere.herokuapp.com/' + 'http://ec.europa.eu/eurostat/wdds/rest/data/v2.1/json/en/teicp270?indic=TOTAL&precision=1&unit=PCH_Q4_NSA').then(function(resp) {
         let data = resp.toTable({ type: 'array' });
         let columns = data.shift();
         let indexes = {};
@@ -167,9 +168,9 @@
         table.appendRows(tableData);
       });
 
-
+      
       //	Gross domestic product, volumes (zmiana % kdk)
-      JSONstat('https://cors-anywhere.herokuapp.com/' + 'http://ec.europa.eu/eurostat/wdds/rest/data/v2.1/json/en/teina011?na_item=B1GQ&precision=1&unit=PCH_Q4_SCA').then(function (resp) {
+      JSONstat('https://cors-anywhere.herokuapp.com/' + 'http://ec.europa.eu/eurostat/wdds/rest/data/v2.1/json/en/teina011?na_item=B1GQ&precision=1&unit=PCH_Q4_SCA').then(function(resp) {
         let data = resp.toTable({ type: 'array' });
         let columns = data.shift();
         let indexes = {};
